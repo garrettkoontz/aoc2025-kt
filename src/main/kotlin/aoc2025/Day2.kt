54351582fad6da65f318456b4f0593df86fc42ce
Garrett Koontz <garrett@k00ntz.com>
2025-12-02 08:20:54 -0500

    Setup day2

---
+++ src/main/kotlin/aoc2025/Day2.kt
@@ -0,0 +1,32 @@
+package com.k00ntz.aoc2025
+
+fun main() {
+    val d = Day2()
+    d.run()
+}
+
+class Day2 {
+    private val fileName = "day2.txt"
+
+    private fun parse(filename: String): List<Rotation> {
+        val raw = ClassLoader.getSystemResourceAsStream(filename)?.bufferedReader()?.readLines()
+        if (raw != null) {
+            return raw.map { Rotation.parse(it) }
+        }
+        throw RuntimeException("unable to find file Day2")
+    }
+
+    fun run() {
+        val input = parse(fileName)
+        println(part1(input))
+        println(part2(input))
+    }
+
+    fun part1(input: List<Rotation>): Int {
+        return TODO()
+    }
+
+    fun part2(input: List<Rotation>): Int {
+        return TODO()
+    }
+}
