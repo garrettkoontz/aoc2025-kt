bd039abc807039eab166dc2437d4eb73fcc4af0a
Garrett Koontz <garrett@k00ntz.com>
2025-12-03 19:35:25 -0500

    Day 2

--- src/main/kotlin/aoc2025/Day2.kt
+++ src/main/kotlin/aoc2025/Day2.kt
@@ -5,15 +5,82 @@ fun main() {
     d.run()
 }
 
+fun isRepeatedTwice(l: Long): Boolean {
+    val s = l.toString()
+    return s.take(s.length / 2) == s.drop(s.length / 2)
+}
 
+fun isRepeated(l: Long): Boolean {
+    val s = l.toString()
+    return (1..s.length / 2).any {
+        if (s.length % it == 0) {
+            val w = s.windowed(it, it)
+            val f = w.first()
+            w.drop(1).all { it == f }
+        } else false
+    }
+}
+
+data class Range(val start: String, val end: String) {
+    companion object {
+        fun parse(s: String): List<Range> =
+            s.split(",").map {
+                val s = it.split("-")
+                Range(s[0], s[1])
+            }
+    }
+
+    fun findInvalidValues(divisor: Int = 2): List<Long> {
+        if (start.length != end.length) {
+            return (start.toLong()..end.toLong()).filter { isRepeatedTwice(it) }
+        } else {
+            val endLong = end.toLong()
+            val startLong = start.toLong()
+            val f = start.take(start.length / divisor)
+            if (f.isNotEmpty() && (f + f).toLong() > end.toLong()) {
+                return emptyList()
+            }
+            val b = end.take(end.length / divisor)
+            return (f.toInt()..b.toInt())
+                .map { it.toString().repeat(divisor).toLong() }
+                .dropWhile { it < startLong }
+                .takeWhile { it <= endLong }
+        }
+    }
+
+    fun findInvalidValues2(takeSize: Int): List<Long> {
+        if (start.length != end.length) {
+            return (start.toLong()..end.toLong()).filter { isRepeated(it) }
+        } else {
+            if (end.length / takeSize == 0) {
+                return emptyList()
+            }
+            val endLong = end.toLong()
+            val startLong = start.toLong()
+            val f = start.take(takeSize)
+            if (f.isNotEmpty() && f.repeat(end.length / takeSize).toLong() > end.toLong()) {
+                return emptyList()
+            }
+            val b = end.take(takeSize)
+            return (f.toInt()..b.toInt())
+                .map { it.toString().repeat(end.length / takeSize).toLong() }
+                .dropWhile { it < startLong }
+                .takeWhile { it <= endLong }
+        }
+    }
+
+    fun findInvalidValues3(): List<Long> {
+        return (start.toLong()..end.toLong()).filter { isRepeated(it) }
+    }
+}
 
 class Day2 {
     private val fileName = "day2.txt"
 
-    private fun parse(filename: String): List<Rotation> {
+    private fun parse(filename: String): List<Range> {
         val raw = ClassLoader.getSystemResourceAsStream(filename)?.bufferedReader()?.readLines()
         if (raw != null) {
-            return raw.map { Rotation.parse(it) }
+            return Range.parse(raw.first())
         }
         throw RuntimeException("unable to find file Day2")
     }
@@ -24,11 +91,27 @@ class Day2 {
         println(part2(input))
     }
 
-    fun part1(input: List<Rotation>): Int {
-         TODO()
+    fun part1(input: List<Range>): Long {
+        val a = input.flatMap {
+            it.findInvalidValues()
+        }
+        return a.sum()
     }
 
-    fun part2(input: List<Rotation>): Int {
-         TODO()
+    fun part2(input: List<Range>): Long {
+        val i = input.flatMap { i ->
+            i.findInvalidValues3()
+        }
+        val j = input.flatMap { i ->
+            (1..i.end.length / 2).flatMap {
+                i.findInvalidValues2(it)
+            }.toSet()
+        }
+        return i.sum()
+
+
     }
 }
+
+//27469618483 too high
+//27469417404
---
+++ src/test/kotlin/aoc2025/Day2Test.kt
@@ -0,0 +1,44 @@
+package aoc2025
+
+import com.k00ntz.aoc2025.Day2
+import com.k00ntz.aoc2025.Range
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day2Test {
+    @Test
+    fun findInvalidValues() {
+        val r1 = Range("1", "12")
+        val r2 = Range("11", "99")
+        val r3 = Range("12342345", "12342346")
+        val r4 = Range("9911", "9998")
+
+        assertEquals(listOf(11L), r1.findInvalidValues())
+        assertEquals(emptyList<Long>(), r3.findInvalidValues())
+        assertEquals(emptyList<Long>(), r4.findInvalidValues())
+        assertEquals(listOf(11L, 22L, 33L, 44L, 55L, 66L, 77L, 88L, 99L), r2.findInvalidValues())
+    }
+
+    @Test
+    fun day2Part1() {
+        val d = Day2()
+        val input = Range.parse(
+            "11-22,95-115,998-1012,1188511880-1188511890,222220-222224," +
+                    "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
+                    "824824821-824824827,2121212118-2121212124"
+        )
+        assertEquals(1227775554, d.part1(input))
+    }
+
+    @Test
+    fun day2Part2() {
+        val d = Day2()
+        val input = Range.parse(
+            "11-22,95-115,998-1012,1188511880-1188511890,222220-222224," +
+                    "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
+                    "824824821-824824827,2121212118-2121212124"
+        )
+        assertEquals(4174379265, d.part2(input))
+    }
+
+}
