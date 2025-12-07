1dd5262ae4c99e883c01142cb3ec776dc1b6ac65
Garrett Koontz <garrett@k00ntz.com>
2025-12-07 13:27:58 -0500

    Day 6

---
+++ src/main/kotlin/com/k00ntz/aoc2025/Day6.kt
@@ -0,0 +1,67 @@
+package com.k00ntz.com.k00ntz.aoc2025
+
+fun main() {
+    val d = Day6()
+    d.run()
+}
+
+enum class Op(val fn: (List<Long>) -> Long) {
+    Plus({ it.sum() }), Times({ it.reduce { acc, i -> acc * i } })
+}
+
+data class Problem(val op: Op, val numbers: List<Long>, val strings: List<String>, val length: Int) {
+    fun solve(): Long {
+        return op.fn(numbers)
+    }
+
+    fun cephalopodSolve(): Long {
+        val extract = (0 until length).map { i -> strings.map { it[i] } }.filterNot { it.all { it == ' ' } }
+        val nums = extract.map { it.joinToString("").trim().toLong() }
+        return op.fn(nums)
+    }
+}
+
+fun parseProblems(lines: List<String>): List<Problem> {
+    val blankIdxs = lines.last().flatMapIndexed { index, ch -> if (ch != ' ') listOf(index) else emptyList() }
+    val ranges = blankIdxs.plus(
+        listOf(lines[0].length)
+    ).zipWithNext()
+    val splits = lines.map { line -> line.split(" ").filterNot { it.isEmpty() } }
+    val idx = splits.first().size
+    return (0 until idx).map { i ->
+        val values = splits.map { it[i] }
+        val op = values.last().let { if (it == "*") Op.Times else Op.Plus }
+        val numbers = values.dropLast(1).map { it.toLong() }
+        Problem(op, numbers, lines.dropLast(1).map { l ->
+            ranges[i].let { idxs ->
+                l.substring(idxs.first, idxs.second)
+            }
+        }, ranges[i].let { it.second - it.first })
+    }
+}
+
+class Day6 {
+    private val fileName = "day6.txt"
+
+    private fun parse(filename: String): List<Problem> {
+        val raw = ClassLoader.getSystemResourceAsStream(filename)?.bufferedReader()?.readLines()
+        if (raw != null) {
+            return parseProblems(raw)
+        }
+        throw RuntimeException("unable to find file Day6")
+    }
+
+    fun run() {
+        val input = parse(fileName)
+        println(part1(input))
+        println(part2(input))
+    }
+
+    fun part1(input: List<Problem>): Long {
+        return input.sumOf { it.solve() }
+    }
+
+    fun part2(input: List<Problem>): Long {
+        return input.sumOf { it.cephalopodSolve() }
+    }
+}
---
+++ src/test/kotlin/com/k00ntz/aoc2025/Day6Test.kt
@@ -0,0 +1,25 @@
+package com.k00ntz.aoc2025
+
+import com.k00ntz.com.k00ntz.aoc2025.Day6
+import com.k00ntz.com.k00ntz.aoc2025.parseProblems
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day6Test {
+    val d = Day6()
+    val input = parseProblems("""123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  """.split("\n"))
+
+    @Test
+    fun part1() {
+        assertEquals(4277556, d.part1(input))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(3263827, d.part2(input))
+    }
+
+}
